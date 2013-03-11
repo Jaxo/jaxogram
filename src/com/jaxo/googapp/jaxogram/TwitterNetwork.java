379cc7c35872f1b40b8706b33b194cc48b48a421
Pierre G. Richard <pgr@jaxo.com>
2013-03-11 12:42:00 +0100

    Set the correct upload method on Twitter (TwitPic)

--- src/com/jaxo/googapp/jaxogram/TwitterNetwork.java
+++ src/com/jaxo/googapp/jaxogram/TwitterNetwork.java
@@ -11,14 +11,20 @@
 */
 package com.jaxo.googapp.jaxogram;
 
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 import java.util.logging.Logger;
 
 import net.oauth.OAuth;
 import net.oauth.OAuthMessage;
-import net.oauth.client.OAuthClient;
+import net.oauth.http.HttpMessage;
+import net.oauth.http.HttpResponseMessage;
+import net.oauth.http.MultipartEntity;
+
 
 // import com.google.orkut.client.api.DefaultOrkutAdapter;
 // import com.google.orkut.client.api.OrkutAdapterDebugListener;
@@ -36,7 +42,8 @@ public class TwitterNetwork extends TwitterAdapter implements Network
    private static final String consumerKey = "1GuuuKkVr2VqZrQRJJ3pkw";
    private static final String consumerSecret = "aWyTrSFTXYBb2ddL67tdb1i8xRGJnNF67c3LkGadFs";
    private static final String UPLOAD_URL = "http://api.twitpic.com/2/upload.json";
-
+   private static final String CREDENTIALS_URL = "https://api.twitter.com/1/account/verify_credentials.json";
+   private static final String REALM_URL = "http://api.twitter.com";
    /*----------------------------------------------------------TwitterNetwork-+
    *//**
    *//*
@@ -107,39 +114,95 @@ public class TwitterNetwork extends TwitterAdapter implements Network
    )
    throws Exception
    {
-      consumer.setProperty(
-         OAuthClient.PARAMETER_STYLE,
-         net.oauth.ParameterStyle.FLICKR_PHOTO_UPLOAD
+      OAuthMessage request = accessor.newRequestMessage(
+         OAuthMessage.GET, CREDENTIALS_URL, null, null
+
       );
-      ArrayList<Map.Entry<String, String>> params;
-      params = new ArrayList<Map.Entry<String, String>>();
-      params.add(new OAuth.Parameter("title", title));
-      params.add(new OAuth.Parameter("description", "Jaxogram")); // FIXME
-      OAuthMessage response = client.invoke(
-         accessor,
+      List<OAuth.Parameter> headers;
+      MultipartEntity entity = new MultipartEntity();
+      entity.addField("key", "7348580b4263e5703fc3cb97e7ba3283", "UTF-8"); // from.getBodyEncoding()
+      entity.addField("message", "Jaxogram Test", "UTF-8");
+      entity.addFile("media", "tmpfile.jpg", "image/jpeg", image);
+      headers = new ArrayList<OAuth.Parameter>();
+      headers.add(
+         new OAuth.Parameter(
+            "X-Verify-Credentials-Authorization",
+            request.getAuthorizationHeader(REALM_URL)
+         )
+      );
+      headers.add(
+         new OAuth.Parameter(
+            "X-Auth-Service-Provider",
+            CREDENTIALS_URL
+         )
+      );
+      headers.add(
+         new OAuth.Parameter(
+            net.oauth.http.HttpMessage.CONTENT_TYPE,
+            entity.getContentType()
+         )
+      );
+      HttpMessage httpRequest = new HttpMessage(
          OAuthMessage.POST,
-         UPLOAD_URL,
-         params,
-         new ByteArrayInputStream(image)
+         new URL(UPLOAD_URL),
+         entity.getBody()
       );
-      String foo = response.readBodyAsString();
-      // Example of JSON response
+      httpRequest.headers.addAll(headers);
+      HttpResponseMessage response = client.getHttpClient().execute(
+         httpRequest,
+         client.getHttpParameters()
+      );
+
+      // return response.readBodyAsString();
+      String foo = toString(
+         response.getBody(), response.getContentCharset()
+      );
+      // Example of JSON responses
       // {
-      //    "id" : "1lad07",
-      //    "text" : "test",
-      //    "url" : "http:\/\/twitpic.com\/1lacuz",
-      //    "width" : 220,
-      //    "height" : 84,
-      //    "size" : 8722,
-      //    "type" : "png",
-      //    "timestamp" : "Wed, 05 May 2010 16:11:48 +0000",
-      //    "user" : {
-      //       "id" : 12345,
-      //       "screen_name" : "twitpicuser"
+      //    "id":"cag6rj",
+      //    "text":"Jaxogram Test",
+      //    "url":"http:\/\/twitpic.com\/cag6rj",
+      //    "width":100,
+      //    "height":100,
+      //    "size":5045,
+      //    "type":"png",
+      //    "timestamp":"Mon, 11 Mar 2013 11:10:16 +0000",
+      //    "user":{
+      //       "id":3186471,
+      //       "screen_name": "jaxosystems"
       //    }
       // }
+      // { "errors":
+      //    [
+      //       {
+      //         "code":401,
+      //         "message":"Could not authenticate you (invalid application key)."
+      //       }
+      //    ]
+      // }
       return foo;
-      // return response.readBodyAsString();
+   }
+
+   /*----------------------------------------------------------------toString-+
+   *//**
+   *//*
+   +-------------------------------------------------------------------------*/
+   public static String toString(InputStream in, String encoding)
+   throws IOException
+   {
+      if (in == null) {
+         return null;
+      }else {
+         try {
+            StringBuilder sb = new StringBuilder();
+            InputStreamReader r = new InputStreamReader(in, encoding);
+            char[] s = new char[512];
+            for (int n; (n = r.read(s)) > 0; sb.append(s, 0, n));
+            return sb.toString();
+         }finally {
+             in.close();
+         }
+      }
    }
 
    /*---------------------------------------------------------------------say-+
