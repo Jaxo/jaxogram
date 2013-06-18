f47605c2a538119f8235c56f64f2f50367747692
Pierre G. Richard <pgr@jaxo.com>
2013-06-18 17:17:36 +0200

    Miscellaneous fixes.  Some clean-up, then... abandon ship!
    "chrome" was definitively too buggy: not understanding data-URL, having problems at
    dispatching the onload event, no toBlob method, bad SVG, etc.  I spent about 2 weeks
    trying hard -- at least that was beneficial to understand more HTML5/SVG. But I don't
    want to waste my time anymore, and I hope that Google will fix their bugs and
    follow the W3C rules.
    
    Consider this branch as defunct.  There were too many changes just trying to have
    "chromium" understand SVG, and those changes started to impact the stability of
    the firefox version.  I'm keeping it for further references...
    
    I created a branch named "CSS_fixes" to repatriate any goodies.

--- pretty/js/filters.js
+++ pretty/js/filters.js
@@ -1,54 +1,35 @@
 var svgHeader = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink";
 
-function makeSvg(imgSrc, filter, w, h) {
+function makeSvg(imgSrc, filter, w, h, isSelfContained) {
    if (filter.vignette.radius) { // if (Object.keys(filter.vignette) !== 0)
-      return vignettize(imgSrc, filter, w, h, false);
+      return vignettize(imgSrc, filter, w, h, isSelfContained);
    }else {
       return (
          svgHeader + "' width='" + w + "' height='" + h + "'>" +
-         makeSvgFiltered(imgSrc, filter, w, h) + "</svg>"
+         filterImage(imgSrc, filter, w, h, isSelfContained) + "</svg>"
       );
    }
 }
 
-function makeSelf(imgSrc, filter, w, h) {   // self-contained
-   if (filter.vignette.radius) { // if (Object.keys(filter.vignette) !== 0)
-      return vignettize(imgSrc, filter, w, h, true);
-   }else {
-      return (
-         svgHeader + "' width='" + w + "' height='" + h + "'>" +
-         makeSelfFiltered(imgSrc, filter, w, h) + "</svg>"
-      );
-   }
-}
-
-function makeSelfFiltered(imgSrc, filter, w, h) {
+function filterImage(imgSrc, filter, w, h, isSelfContained) {
    if (filter.value) {
-      return (
-         "<g>" +
-         declareImage(imgSrc, w, h) +
-         "' filter='url(data:image/svg+xml," +
-         escape("<svg xmlns='http://www.w3.org/2000/svg'>" + declareFilter(filter) + "</svg>") +
-         "#" + filter.name + ")'></image></g>"
-      );
+      if (isSelfContained) {
+         return declareImage(
+            imgSrc, w, h,
+            dataUrlize("filter", declareFilter(filter), filter.name)
+         );
+      }else {
+         return (
+            declareFilter(filter) +
+            declareImage(imgSrc, w, h, "' filter='url(#" + filter.name + ")")
+         );
+      }
    }else {
-      return "<g>" + declareImage(imgSrc, w, h) + "'></image></g>"
+      return declareImage(imgSrc, w, h, "");
    }
 }
 
-function makeSvgFiltered(imgSrc, filter, w, h) {
-   if (filter.value) {
-      return (
-         declareFilter(filter) +
-         declareImage(imgSrc, w, h) +
-         "' filter='url(#" + filter.name + ")'></image>"
-      );
-   }else {
-      return declareImage(imgSrc, w, h) + "'></image>"
-   }
-}
-
-function vignettize(imgSrc, filter, w, h, selfContained) {
+function vignettize(imgSrc, filter, w, h, isSelfContained) {
    var f1 = filter.name + "_2";
    var f2 = filter.name + "_3";
    var f3 = filter.name + "_4";
@@ -68,45 +49,46 @@ function vignettize(imgSrc, filter, w, h, selfContained) {
       p = h/w;
       q = 1.0;
    }
-   var border = (m * 0.0214) | 0;
-   if (border < 2) border = 2;
-   if (selfContained) {
-      return (
-         svgHeader + "' width='" + m + "' height='" + m +
-         "'><g transform='translate(" + t + "," + u + ")'>" +
-         makeSelfFiltered(imgSrc, filter, w, h) +
-         "<g>" +
-         z0(imgSrc, w, h, zz1(f1) + zz3(f3, zz2(f2, p, q, r))) +
-         "</g>" + z5(border, m, t, u) +
-         "</g></svg>"
+   var thick = (m * 0.0214) | 0;
+   if (thick < 2) thick = 2;
+
+   var vignette;
+   if (isSelfContained) {
+      vignette = declareImage(
+         imgSrc, w, h,
+         dataUrlize("filter", declareBlur(f1, b), f1) +
+         dataUrlize(
+            "mask",
+            declareMask(
+               f3,
+               dataUrlize("fill", declareFill(f2, p, q, r), f2)
+            ), f3
+         )
       );
    }else {
-      return (
-         svgHeader + "' width='" + m + "' height='" + m +
-         "'><g transform='translate(" + t + "," + u + ")'>" +
-         makeSvgFiltered(imgSrc, filter, w, h) +
-         "<g>" +
-         z1(f1, b) +
-         z2(f2, p, q, r) +
-         z3(f3, "' fill='url(#" + f2 + ")") +
-         z0(imgSrc, w, h, "' filter='url(#" + f1 + ")' mask='url(#" + f3 + ")") +
-         "</g>" + z5(border, m, t, u) +
-         "</g></svg>"
+      vignette = (
+         declareBlur(f1, b) +
+         declareFill(f2, p, q, r) +
+         declareMask(f3, "' fill='url(#" + f2 + ")") +
+         declareImage(
+            imgSrc, w, h,
+            "' filter='url(#" + f1 + ")' mask='url(#" + f3 + ")"
+         )
       );
    }
-}
-
-function declareImage(imgSrc, w, h) {
    return (
-      "<image width='" + w + "' height='" + h +
-      "' preserveAspectRatio='xMinYMin meet' xlink:href='" + imgSrc
+      svgHeader + "' width='" + m + "' height='" + m +
+      "'><g transform='translate(" + t + "," + u + ")'>" +
+      filterImage(imgSrc, filter, w, h, isSelfContained) +
+      "<g>" + vignette + "</g>" + doBorder(thick, m, t, u) + "</g></svg>"
    );
 }
+
 function declareFilter(filter) {
    return "<filter id='" + filter.name + "'>" + filter.value + "</filter>";
 }
 
-function z0(imgSrc, w, h, attrs) {
+function declareImage(imgSrc, w, h, attrs) {
    return (
       "<image xlink:href='" + imgSrc +
       "' width='" + w +
@@ -116,7 +98,7 @@ function z0(imgSrc, w, h, attrs) {
    );
 }
 
-function z1(f, b) {
+function declareBlur(f, b) {
    return (
       "<filter id='" + f + "'>" +
       "<feGaussianBlur stdDeviation='2'/>" +
@@ -124,14 +106,8 @@ function z1(f, b) {
       "</filter>"
    );
 }
-function zz1(f, b) {
-   return (
-      "' filter='url(data:image/svg+xml," +
-      escape("<svg xmlns='http://www.w3.org/2000/svg'>" + z1(f, b) + "</svg>") +
-      "#" + f + ")"
-   );
-}
-function z2(f, p, q, r) {
+
+function declareFill(f, p, q, r) {
    return (
       "<radialGradient id='" + f +
       "' gradientUnits='objectBoundingBox' cx='" + (1/(2*p)) +
@@ -142,14 +118,8 @@ function z2(f, p, q, r) {
       "</radialGradient>"
    );
 }
-function zz2(f, p, q, r) {
-   return (
-      "' fill='url(data:image/svg+xml," +
-      escape("<svg xmlns='http://www.w3.org/2000/svg'>" + z2(f, p, q, r) + "</svg>") +
-      "#" + f + ")"
-   );
-}
-function z3(f, value) {
+
+function declareMask(f, value) {
    return (
       "<mask id='" + f +
       "' maskUnits='objectBoundingBox' maskContentUnits='objectBoundingBox'>" +
@@ -157,81 +127,22 @@ function z3(f, value) {
       value + "'/></mask>"
    );
 }
-function zz3(f, value) {
+
+function doBorder(thick, m, t, u) {
    return (
-      "' mask ='url(data:image/svg+xml," +
-      escape("<svg xmlns='http://www.w3.org/2000/svg'>" + z3(f, value)  + "</svg>") +
-      "#" + f + ")"
-   );
-}
-function z5(border, m, t, u) {
-   return (
-      "<g fill='none' stroke='white' stroke-width='" + border + "' >" +
-        "<rect x='" + ((border/2)-t) +
-        "' y='" + ((border/2)-u) +
-        "' width='" + (m-border) +
-        "' height='" + (m-border) + "'/>" +
+      "<g fill='none' stroke='white' stroke-width='" + thick + "' >" +
+        "<rect x='" + ((thick/2)-t) +
+        "' y='" + ((thick/2)-u) +
+        "' width='" + (m-thick) +
+        "' height='" + (m-thick) + "'/>" +
       "</g>"
    );
 }
 
-// var elt = document.getElementById("ft6");     OBSO
-// for (var i=0, max=filters.length; i < max; ++i) {
-//    var tdElt = document.createElement("TD");
-//    var imgElt = document.createElement("IMG");
-//    imgElt.onload = function() {
-//      // no longer need to read the blob so it's revoked
-//    };
-//    filters[i].thumbImg = imgElt;
-//    tdElt.setAttribute("align", "center");
-//    tdElt.appendChild(imgElt);
-//    elt.appendChild(tdElt);
-// }
-
-// var thumbMaxWidth = 0;
-// var thumbMaxHeight = 0;
-// showToolbar(2);
-// thumbMaxWidth = (elt.cells)[0].offsetWidth;
-// thumbMaxHeight = (elt.cells)[0].offsetHeight;
-// showToolbar(0);
-
-//             var rawThumb = URL.createObjectURL(thumbBlob);
-//             filters[0].thumbImg.src = rawThumb;
-//             for (var i=1, max=filters.length; i < max; ++i) {
-//                filters[i].thumbImg.src = URL.createObjectURL(
-//                   doFilter(w, h, rawThumb, filters[i])
-//                );
-//             }
-//
-// function fitImages() {
-//    // workaround, until "object-fit:contain;" gets implemented
-//    var images = document.querySelectorAll(".imgbox img");
-//    for (var i=0; i < images.length; ++i) {
-//       var img = images[i];
-//       img.addEventListener("load", function() { fitImage(this); });
-//       fitImage(img);
-//    }
-// }
-//
-// function fitImage(img) {
-//    var elt = img.parentNode;
-//    if ((elt.offsetHeight * img.offsetWidth)<(img.offsetHeight * elt.offsetWidth)) {
-//       img.style.top = "0";
-//       img.style.width = "";
-//       img.style.height = "100%";
-//    }else {
-//       img.style.top = ((elt.offsetHeight - img.offsetHeight) / 2) + "px";
-//       img.style.width = "100%";
-//       img.style.height = "";
-//    }
-// }
-//
-//    // 0) Set the filters
-//    for (var i=1, max=filters.length; i < max; ++i) {
-//       var filter = filters[i];
-//       if (filter.src) URL.revokeObjectURL(filter.src);
-//       filter.src = URL.createObjectURL(
-//          doFilter(img.width, img.height, img.src, filter)
-//       );
-//   }
-
+function dataUrlize(attname, svgVal, f) {
+   return(
+      "' " + attname + "='url(data:image/svg+xml," +
+      escape("<svg xmlns='http://www.w3.org/2000/svg'>" + svgVal + "</svg>") +
+      "#" + f + ")"
+   );
+}
