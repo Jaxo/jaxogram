75f5661c5fb42b0f970059d84b330d7644e026ae
Pierre G. Richard <pgr@jaxo.com>
2013-08-09 07:49:09 +0200

    First pages commit

---
+++ filterFactory/FilesList.js
@@ -0,0 +1,269 @@
+function FilesList(replacedId, title) {
+   var tsegment;
+   var td;
+   var tr;
+   var btn;
+   var that = this;
+   var elt = document.getElementById(replacedId);
+   tsegment = document.createElement("THEAD");
+   tr = document.createElement("TR");
+   td = document.createElement("TH");
+   td.setAttribute("colspan", "4");
+   td.innerHTML = elt.innerHTML;
+   tr.appendChild(td);
+   tsegment.appendChild(tr);
+   tr = document.createElement("TR");
+   td = document.createElement("TH");
+   td.setAttribute("colspan", "4");
+   td.appendChild(document.createElement("HR"));
+   tr.appendChild(td);
+   tsegment.appendChild(tr);
+   tr = document.createElement("TR");
+   td = document.createElement("TH");
+   td.textContent = "Name";
+   tr.appendChild(td);
+   td = document.createElement("TH");
+   td.textContent = "Size";
+   tr.appendChild(td);
+   td = document.createElement("TH");
+   td.textContent = "Last Modified";
+   td.setAttribute("colspan", "2");
+   tr.appendChild(td);
+   tsegment.appendChild(tr);
+
+   this.tableInner = document.createElement("TABLE");
+   this.tableInner.className = "fl_TableInner";
+   this.tableInner.appendChild(tsegment);  // THEAD
+   this.tableBody = document.createElement("TBODY");
+   this.tableInner.appendChild(this.tableBody);
+
+   tsegment = document.createElement("TFOOT");
+   tr = document.createElement("TR");
+   td = document.createElement("TD");
+   td.setAttribute("colspan", "4");
+   this.buttons = td;
+   tr.appendChild(td);
+   tsegment.appendChild(tr);
+   this.tableInner.appendChild(tsegment);
+
+   this.addButton("Cancel").onclick = function() { that.clear(); }
+   this.tableInner.onclick = function(event) {
+      var elt = event.target;      // the item that was clicked
+      while ((elt != null) && (elt.nodeName != "TR")) {
+         elt = elt.parentNode;
+      }
+      if (elt && (elt.className === "file")) {
+         event.preventDefault();
+         event.stopPropagation();
+         that.clicked(event, elt.rowIndex);
+      }
+   }
+
+   tr = document.createElement("TR");
+   td = document.createElement("TD");
+   td.setAttribute("align", "center");
+   td.appendChild(this.tableInner);
+   tr.appendChild(td);
+   tsegment = document.createElement("TBODY");
+   tsegment.appendChild(tr);
+
+   this.tableOuter = document.createElement("TABLE");
+   this.tableOuter.className = "fl_TableContainer";
+   this.tableOuter.appendChild(tsegment);
+
+   var dad = elt.parentNode;
+   var sib = elt.nextSibling;
+   dad.removeChild(elt);
+   dad.insertBefore(this.tableOuter, sib);
+
+   // Selections...
+   // this.bkgds = new Array("#eaeaea", "#ffffff", "#fbb7ec", "#fad3f2");
+   this.bkgds = new Array("transparent", "transparent", "#f07746", "#f07746");
+   this.fggds = new Array("black", "black", "white", "white");
+
+   this.firstRow = -1;
+   this.lastRow = -1;
+   this.plusRows = [];
+   this.minusRows = [];
+}
+
+FilesList.prototype = {
+   addButton: function(name) {
+      btn = document.createElement("BUTTON");
+      btn.style = "margin-left:1rem";
+      btn.textContent = name;
+      this.buttons.appendChild(btn);
+      return btn;
+   },
+   populate: function(result) {
+      var row;
+      var cell0;
+      var cell1;
+      var cell2;
+      var cell3;
+      var date;
+      var split;
+      var tblBody = this.tableBody;
+      tblBody.innerHTML = "";
+      result.forEach(
+         function(entry) {
+            row = document.createElement("TR");
+            row.className = "file";
+            cell0 = document.createElement("TD");
+            cell1 = document.createElement("TD");
+            cell2 = document.createElement("TD");
+            cell3 = document.createElement("TD");
+            date = entry.creation;
+            split = date.lastIndexOf(' ', date.lastIndexOf(' ')-1);
+            cell0.textContent = entry.name;
+            cell1.textContent = entry.size;
+            // Date is deemed to be as in: "Wed, 01 May 2013 20:14:37 GMT"
+            cell2.textContent = date.substr(0, split);
+            cell3.textContent = date.substr(split+1);
+            row.appendChild(cell0);
+            row.appendChild(cell1);
+            row.appendChild(cell2);
+            row.appendChild(cell3);
+            tblBody.appendChild(row);
+         }
+      );
+      this.tableOuter.style.visibility = "visible";
+   },
+   clear: function() {
+      this.tableOuter.style.visibility = "hidden";
+      this.reset();
+   },
+   /* Selections... */
+   clicked: function(ev, newRow) {
+      if (ev.ctrlKey) {
+         if (this.firstRow < 0) {
+            this.firstRow = newRow;
+            this.setBkgd(newRow, 2);
+         }else if (
+            (newRow == this.firstRow) || (
+               (this.lastRow >= 0) && (
+                  ((newRow >= this.firstRow) && (newRow <= this.lastRow)) ||
+                  ((newRow >= this.lastRow) && (newRow <= this.firstRow))
+               )
+            )
+         ) {
+            ix = this.minusRows.indexOf(newRow);
+            if (ix >= 0) {
+               this.setBkgd(this.minusRows[ix], 2);
+               this.minusRows.splice(ix, 1);
+            }else {
+               this.setBkgd(newRow, 0);
+               this.minusRows.push(newRow);
+            }
+         }else {
+            ix = this.plusRows.indexOf(newRow);
+            if (ix >= 0) {
+               this.setBkgd(this.plusRows[ix], 0);
+               this.plusRows.splice(ix, 1);
+            }else {
+               this.setBkgd(newRow, 2);
+               this.plusRows.push(newRow);
+            }
+         }
+      }else if (ev.shiftKey) {
+         this.clearExtraRows();
+         if (this.firstRow != newRow) {
+            if (this.firstRow < 0) {
+               this.setBkgd(newRow, 2);
+               this.firstRow = newRow;
+            }else {
+               this.setBkgd(this.firstRow, 2);
+               this.fillRows(0);
+               this.lastRow = newRow;
+               this.fillRows(2);
+            }
+         }
+      }else {
+         this.clearExtraRows();
+         if (this.firstRow >= 0) {
+            this.setBkgd(this.firstRow, 0);
+            this.fillRows(0);
+            this.lastRow = -1;
+         }
+         this.firstRow = newRow;
+         this.setBkgd(newRow, 2);
+      }
+      return false;
+   },
+   reset: function() {
+      this.clearExtraRows();
+      if (this.firstRow >= 0) {
+         this.setBkgd(this.firstRow, 0);
+         this.fillRows(0);
+      }
+      this.firstRow = -1;
+      this.lastRow = -1;
+   },
+   clearExtraRows: function() {
+      this.minusRows.length = 0;
+      var len = this.plusRows.length;
+      for (ix=0; ix < len; ++ix) {
+         this.setBkgd(this.plusRows[ix], 0);
+      }
+      this.plusRows.length = 0;
+   },
+   fillRows: function(ixBkgd) {
+      // assert: this.firstRow >= 0
+      if (this.lastRow >= 0) {
+         if (this.lastRow >= this.firstRow ) {
+            i = this.firstRow;
+            while (++i <= this.lastRow) {
+               this.setBkgd(i, ixBkgd);
+            }
+         }else {
+            i = this.firstRow;
+            while (--i >= this.lastRow) {
+               this.setBkgd(i, ixBkgd);
+            }
+         }
+      }
+   },
+   getSelectedRows: function() {
+      // 3 is the # of rows in the THEAD.  Yes, it's a bit hacky.
+      var result = [];
+      var i;
+      if (this.firstRow >= 0) {
+         if (this.minusRows.indexOf(this.firstRow) == -1) {
+            result.push(this.firstRow - 3);
+         }
+         if (this.lastRow >= 0) {
+            if (this.lastRow >= this.firstRow ) {
+               i = this.firstRow;
+               while (++i <= this.lastRow) {
+                  if (this.minusRows.indexOf(i) == -1) {
+                     result.push(i-3);
+                  }
+               }
+            }else {
+               i = this.firstRow;
+               while (--i >= this.lastRow) {
+                  if (this.minusRows.indexOf(i) == -1) {
+                     result.push(i-3);
+                  }
+               }
+            }
+         }
+      }
+      for (i=0; i < this.plusRows.length; ++i) {
+         result.push(this.plusRows[i]-3);
+      }
+      return result;
+   },
+   getCellValue: function(rowNo, cellNo) {
+      // 3 is the # of rows in the THEAD.  Yes, it's a bit hacky.
+      return this.tableInner.rows[rowNo+3].cells[cellNo].innerHTML;
+   },
+   setBkgd: function(rowNo, ixBkgd) {
+      if ((rowNo & 1) > 0) ++ixBkgd;
+      // this.tableInner.rows[rowNo].background = this.bkgds[ixBkgd];
+      var style = this.tableInner.rows[rowNo].style;
+      style.background = this.bkgds[ixBkgd];
+      style.color = this.fggds[ixBkgd];
+   }
+}
+
---
+++ filterFactory/filters.js
@@ -0,0 +1,416 @@
+var currentFilter;
+var xRW = 0.3086;
+var xGW = 0.6094;
+var xBW = 0.0820;
+var RW = 0.213;
+var GW = 0.715;
+var BW = 0.072;
+var RX = 0.787;  // 1-RW
+var GX = 0.285;  // 1-GW
+var BX = 0.928;  // 1-BW
+var RA = 0.143;
+var GA = 0.140;
+var BA = 0.283;
+
+var filters = [
+   "brightness", "saturation", "rotateHue",
+   "contrast", "blur", "sepia", "dusk", "sharpen", "sharpenChk",
+   "extra2", "extra3", "noir", "moat",
+   "vignetteChk", "vigRadius", "vigBright"
+
+];
+
+function initFilters() {
+   filters.forEach(
+      function(name) {
+         var elt = document.getElementById(name);
+         if (elt.type === "checkbox") {
+            elt.addEventListener('change', filterImage);
+         }else if (elt.type === "range") {
+            elt.addEventListener('input', filterImage);
+         }
+      }
+   );
+   var vertVal = document.getElementById("vertVal");
+   document.querySelector(".vsliders").onmouseout = function() {
+      vertVal.style.display = "none";
+   }
+   var vertSliders = document.querySelectorAll("input[orient='vertical']");
+   for (var i=0, max=vertSliders.length; i < max; ++i) {
+      var elt = vertSliders[i];
+      elt.onmouseover = function() {
+         vertVal.style.display = "";
+         vertVal.textContent = this.value;
+      }
+      elt.setAttribute("ix", i);
+      elt.oninput = function() {
+         vertVal.textContent = this.value;
+         currentFilter.updateColorsLevels(this.getAttribute("ix"), this.value);
+         filterImage();
+      }
+   }
+   currentFilter = new Filter();
+}
+
+function multiply(a, b) {
+   var t = 0;
+   var res = new Float64Array(20);
+   for (var i=0, t=0; t < 20; i += 5) {
+      for (var j=0; j < 5; ++j, ++t) {
+         res[t] = 0;
+         for (var k=0, l=i, m=j; k < 4; ++k, ++l, m += 5) {
+            res[t] += a[l] * b[m];
+         }
+      }
+      res[t-1] += a[i+4];
+   }
+   return res;
+}
+
+function saturate(s, vals) {
+   if (s == 100) {
+      return vals;
+   }else {
+      s /= 100;
+      return multiply(
+         vals,
+         [
+            (((1.0-s)*RW) + s), (((1.0-s)*GW)), (((1.0-s)*BW)), 0, 0,
+            (((1.0-s)*RW)), (((1.0-s)*GW) + s), (((1.0-s)*BW)), 0, 0,
+            (((1.0-s)*RW)), (((1.0-s)*GW)), (((1.0-s)*BW) + s), 0, 0,
+            0, 0, 0, 1, 0
+         ]
+      );
+   }
+}
+
+function rotateHue(d, vals) {
+   var rad = (d * Math.PI) / 180;
+   var cos = Math.cos(rad);
+   var sin = Math.sin(rad);
+   return multiply(
+      vals,
+      [
+         RW+(cos*RX)-(sin*RW), GW-(cos*GW)-(sin*GW), BW-(cos*BW)+(sin*BX), 0, 0,
+         RW-(cos*RW)+(sin*RA), GW+(cos*GX)+(sin*GA), BW-(cos*BW)-(sin*BA), 0, 0,
+         RW-(cos*RW)-(sin*RX), GW-(cos*GW)+(sin*GW), BW+(cos*BX)+(sin*BW), 0, 0,
+         0, 0, 0, 1, 0
+      ]
+   );
+}
+function doExtra3(d, vals) {
+   if (d != 100) {
+      d /= 100;
+      vals[0] *= (1-RW) * d;
+      vals[6] *= (1-GW) * d;
+      vals[11] *= (1-BW) * d;
+   }
+   return vals;
+}
+
+function doDusk(dusk, vals) {
+   if (!dusk) {
+      return vals;
+   }else {
+      return multiply(
+         vals,
+         [
+            1.0, 0, 0, 0, 0,
+            0, 0.2, 0, 0, 0,
+            0, 0, 0.2, 0, 0,
+            0, 0, 0, 1, 0
+         ]
+      );
+   }
+}
+
+function doSepia(sepia, vals) {
+   if (!sepia) {
+      return vals;
+   }else {
+      return multiply(
+         vals,
+         [
+            0.280, 0.450, 0.05, 0, 0,
+            0.140, 0.390, 0.04, 0, 0,
+            0.080, 0.280, 0.03, 0, 0,
+            0, 0, 0, 1, 0
+         ]
+      );
+   }
+}
+
+function makeGaussianBlur() {
+   var blur = currentFilter.blur;
+   if (blur == 0) {
+      return "";
+   }else {
+      return "<feGaussianBlur stdDeviation='" + (blur/10) + "'/>";
+   }
+}
+
+function makeFilterNoir() {
+   if (currentFilter.noir) {
+      return (
+         "<feGaussianBlur stdDeviation='1.5'/>" +
+         "<feComponentTransfer>" +
+           "<feFuncR type='discrete' tableValues='0 .5 1 1'/>" +
+           "<feFuncG type='discrete' tableValues='0 .5 1'/>" +
+           "<feFuncB type='discrete' tableValues='0'/>" +
+         "</feComponentTransfer>"
+      );
+   }else {
+      return "";
+   }
+}
+
+function makeFilterMoat() {
+   if (currentFilter.moat) {
+      return (
+         "<feConvolveMatrix order='5'" +
+         " kernelMatrix='" +
+         "1  1    1     1   1 " +
+         "1 -1.2 -1.2  -1.2 1 " +
+         "1 -1.2  -3   -1.2 1 " +
+         "1 -1.2 -1.2  -1.2 1 " +
+         "1  1  1  1  1'/>"
+      );
+   }else {
+      return "";
+   }
+}
+
+function makeSharpen() {
+   if (currentFilter.isSharpened) {
+      return (
+         "<feConvolveMatrix order='3'" +
+         " kernelMatrix='" +
+         " 1 -1  1 " +
+         "-1 " + -(currentFilter.sharpen/100) + " -1 " +
+         " 1 -1  1'/>"
+      );
+   }else {
+      return "";
+   }
+}
+
+function makeContrast() {
+   var contrast = (currentFilter.contrast/2) - 50;
+   var slope;
+   var intercept;
+   if (contrast == 0) {
+      return "";
+   }else if (contrast < 0) {
+      contrast = -contrast;
+      contrast /= 100;
+      intercept = contrast;
+      slope = 1 - (2*contrast);
+   }else {
+      contrast /= 200;
+      slope = 1 / (1 - (2*contrast));
+      intercept = (1 - slope) / 2;
+   }
+   slope = slope.toFixed(4);
+   intercept = intercept.toFixed(4);
+   return (
+      "<feComponentTransfer>" +
+      "<feFuncR type='linear' slope='" + slope +
+      "' intercept='" + intercept + "'/>" +
+      "<feFuncG type='linear' slope='" + slope +
+      "' intercept='" + intercept + "'/>" +
+      "<feFuncB type='linear' slope='" + slope +
+      "' intercept='" + intercept + "'/>" +
+      "</feComponentTransfer>"
+   );
+}
+
+function makeColorsLevels() {
+   var levels;
+   var value = "";
+   levels = currentFilter.redLevels();
+   if (levels !== "") {
+      value += "<feFuncR type='table' tableValues='" + levels + "'/>";
+   }
+   levels = currentFilter.greenLevels();
+   if (levels !== "") {
+      value += "<feFuncG type='table' tableValues='" + levels + "'/>";
+   }
+   levels = currentFilter.blueLevels();
+   if (levels !== "") {
+      value += "<feFuncB type='table' tableValues='" + levels + "'/>";
+   }
+   if (value !== "") {
+      value = "<feComponentTransfer>" + value + "</feComponentTransfer>"
+   }
+   return value;
+}
+
+function makeColorMatrix() {
+   var brightness = currentFilter.brightness / 100;
+   var vals = [
+      brightness, 0, 0, 0, 0,
+      0, brightness, 0, 0, 0,
+      0, 0, brightness, 0, 0,
+      0, 0, 0, 1, 0
+   ];
+   vals = saturate(currentFilter.saturation, vals);
+   vals = rotateHue(currentFilter.hueRot, vals);
+   vals = doExtra3(currentFilter.extra3, vals);
+   vals = doSepia(currentFilter.sepia, vals);
+   vals = doDusk(currentFilter.dusk, vals);
+   var isIdentity = true;
+   var filterValue = "";
+   for (var i=0, j=-1; i < 20; ++i) {
+      var v = vals[i];
+      if ((i%5) == 0) ++j;
+      if ((i%5) == j) { // diagonal
+         if (v != 1) isIdentity = false;
+      }else {
+         if (v != 0) isIdentity = false;
+      }
+      filterValue += " " + vals[i].toFixed(4);
+   }
+   if (!isIdentity) {
+      return "<feColorMatrix type='matrix' values='" +  filterValue + "'/>"
+   }else {
+      return ""; // no need for.
+   }
+}
+
+function filterImage() {
+   currentFilter.refresh();
+   // alert(JSON.stringify(currentFilter));
+   var filterValue = (
+      makeColorsLevels() +
+      makeColorMatrix() +
+      makeContrast() +
+      makeGaussianBlur() +
+      makeFilterNoir() +
+      makeFilterMoat() +
+      makeSharpen()
+   );
+   var originalImage = document.getElementById("originalImage");
+   var filteredImage = document.getElementById("filteredImage");
+   currentFilter.data = {};
+   currentFilter.data.name = filesmanager.currentFileName();
+   currentFilter.data.value = filterValue;
+   currentFilter.data.vignette = {};
+
+   if ((filterValue === "") && !currentFilter.isVignetted) {
+      filteredImage.src = originalImage.src;
+   }else {
+      var w = originalImage.width;    // 450
+      var h = originalImage.height;   // 281
+      var baseFilter = (
+         "<g><image xlink:href='" + originalImage.src +
+         "' width='" + w + "' height='" + h
+      );
+      if (filterValue !== "") {
+         baseFilter += (
+            "' filter='url(data:image/svg+xml," +
+            escape(
+               "<svg xmlns='http://www.w3.org/2000/svg'><filter id='f1'>" +
+               filterValue +
+               "</filter></svg>"
+            ) +
+            "#f1)"
+         );
+      }
+      baseFilter += "'></image></g>";
+      var svg;
+      if (!currentFilter.isVignetted) {
+         svg = (
+            "<svg width='" + w + "' height='" + h +
+            "' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>" +
+            baseFilter +
+            "</svg>"
+         );
+      }else {
+//       document.getElementById("filterValue").innerHTML = filterValue;
+         currentFilter.data.vignette.radius = currentFilter.vigRadius;
+         currentFilter.data.vignette.bright = currentFilter.vigBright;
+         svg = vignetize(
+            baseFilter, originalImage,
+            currentFilter.vigRadius/100, currentFilter.vigBright/100
+         );
+      }
+      filteredImage.src = URL.createObjectURL(
+         new Blob([svg], {type:"image/svg+xml"})
+      );
+   }
+}
+
+function vignetize(baseFilter, originalImage, r, b)
+{
+   var f2 = "f2";
+   var f3 = "f3";
+   var f4 = "f4";
+   // var border = 6;
+   var w = originalImage.width;    // 450
+   var h = originalImage.height;   // 281
+   var m, t, u, p, q, cx, cy;
+   if (h < w) {
+      m = h;
+      t = (h-w)/2;       // -84
+      u = 0;             // 0
+      p = 1.0;
+      q = w/h;           // 1.6014
+   }else {
+      m = w;
+      t = 0;
+      u = (w-h)/2;
+      p = h/w;
+      q = 1.0;
+   }
+   var border = (m * 0.0214) | 0;
+   return (
+     "<svg width='" + m + "' height='" + m +
+     "' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>" +
+       "<g transform='translate(" + t + "," + u + ")'>" +
+         baseFilter +
+         "<g>" +
+            "<image xlink:href='" + originalImage.src +
+            "' width='" + w + "' height='" + h +
+            "' filter='url(data:image/svg+xml," +
+            escape(
+               "<svg xmlns='http://www.w3.org/2000/svg'><filter id='" + f2 + "'>" +
+               "<feGaussianBlur stdDeviation='2'/>" +
+               "<feColorMatrix type='matrix' values='" + b + " 0 0 0 0 0 " + b + " 0 0 0 0 0 " + b + " 0 0 0 0 0 1 0'/>" +
+               "</filter></svg>"
+            ) +
+            "#" + f2 +
+            ")' mask ='url(data:image/svg+xml," +
+            escape(
+               "<svg xmlns='http://www.w3.org/2000/svg'>" +
+               "<mask id='" + f3 +
+               "' maskUnits='objectBoundingBox' maskContentUnits='objectBoundingBox'>" +
+               "<rect x='0' y='0' width='1' height='1' stroke-width='0'" +
+               " fill='url(data:image/svg+xml," +
+               escape(
+                  "<svg xmlns='http://www.w3.org/2000/svg'><radialGradient id='" + f3 + "'" +
+                     " gradientUnits='objectBoundingBox' cx='" + (1/(2*p)) + "' cy='" + (1/(2*q)) + "' r='" + r + "'" +
+                     " gradientTransform='scale(" + p + "," + q + ")'" +
+                  ">" +
+                     "<stop offset='0.4' stop-color='#ffffff' stop-opacity='0'/>" +
+                     "<stop offset='0.6' stop-color='#ffffff' stop-opacity='1'/>" +
+                  "</radialGradient></svg>"
+               ) +
+               "#" + f3 + ")'/>" +
+               "</mask></svg>"
+            ) +
+            "#" + f3 + ")'></image>" +
+         "</g>" +
+         "<g fill='none' stroke='white' stroke-width='" + border + "' >" +
+           "<rect x='" + ((border/2)-t) + "' y='" + ((border/2)-u) + "' width='" + (m-border) + "' height='" + (m-border) + "'/>" +
+         "</g>" +
+       "</g>" +
+     "</svg>"
+   );
+}
+
+function doDebug() {
+   var db = document.getElementById("debugData");
+   db.onclick = function() {
+      alert(JSON.stringify(currentFilter.data));
+   }
+}
